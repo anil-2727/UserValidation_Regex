25c0ea0fe71e2968844dab7d4152c7886fc40e3c
anil-2727 <bali39959@gmail.com>
2021-07-14 17:51:22 +0530

    added:functionality to validate firstname

--- src/com/bridgelabz/validation/UserValidator.java
+++ src/com/bridgelabz/validation/UserValidator.java
@@ -1,9 +1,24 @@
 package com.bridgelabz.validation;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserValidator {
 
 	public static void main(String[] args) {
+		UserValidator validator = new UserValidator();
 		System.out.println("Welcom to Validator");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the FirstName");
+		String firstName = scanner.next();
+		boolean nameStatus = validator.validateFirstName(firstName);
+		System.out.println("First name validation " + nameStatus);
+
+	}
+
+	public boolean validateFirstName(String firstName) {
+		return (Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", firstName));
+
 	}
 
 }
